b722e37292671cb58c37af91fb1e9bc5c1ae5f05
Voidemprr <ilhamsabillah09@gmail.com>
2026-01-11 22:07:18 +0800

    Implement game loop and menu in main function
    Refactor main function to include a game loop and menu options.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Welcome tu de Logic Game!" << endl;
-    // ini apa yak, ininya jangan di iniin gitu nah
-    int input = 0;
-    cout << "jawab: " << flush;
-    cin >> input;
-    if (input == 1) {
-        cout << "Informatics" << endl;
-    } else if (input == 2) {
-        cout << "Logic" << endl;
-    } else {
-        cout << "coming soon" << endl;
-    }    return 0;
+int main()
+{
+
+    bool gameloop = true;
+    while (gameloop)
+    {
+        cout << "Welcome tu de Logic Game!" << endl;
+        // ini apa yak, ininya jangan di iniin gitu nah
+        int choice = 0;
+        cout << "menu: " << flush;
+        cin >> choice;
+        if (choice == 1)
+        {
+            cout << "Informatics" << endl;
+        }
+        else if (choice == 2)
+        {
+            cout << "Logic" << endl;
+        }
+        else if (choice == 3)
+        {
+            cout << "Math" << endl;
+        }
+        else if (choice == 4)
+        {
+            gameloop = false;
+        }
+        else
+        {
+            cout << "coming soon" << endl;
+        }
+    }
+
+    return 0;
 }
